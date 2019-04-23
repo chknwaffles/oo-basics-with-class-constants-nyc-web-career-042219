ad652095ac257ee54a647d7e2c9fa14f56fb1681
Won Kim <wonsae.kim@gmail.com>
2019-04-23 14:21:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
   GENRES = []
@@ -11,6 +11,7 @@ class Book
   def genre=(book_genre)
     @genre = book_genre
   end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
